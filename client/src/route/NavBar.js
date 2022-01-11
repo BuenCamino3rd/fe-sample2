5ee8eb21a8f8332f1edcd2ecbf8372f382e2247a
0andme <yu00mi97@gmail.com>
2022-01-11 15:04:53 +0900

    feat : 라우터 컴포넌트 연결

--- client/src/route/NavBar.js
+++ client/src/route/NavBar.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Home from "../components/home/Home";
+import Gather from "../components/gather/Gather";
+import Save from "../components/save/Save";
+import Compete from "../components/compete/Compete";
+import Profile from "../components/profile/Profile";
+import Settings from "../components/settings/Settings";
+import Alerts from "../components/alerts/Alerts";
+import App from "../App";
 
 function NavBar() {
   return (
     <>
       <BrowserRouter>
+        <Link to="/home">홈</Link>
+        <Link to="/gather">모으기</Link>
+        <Link to="/save">아끼기</Link>
+        <Link to="/compete">챌린지</Link>
+        <Link to="/profile">프로필</Link>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/gather" element={<Gather />} />
@@ -15,7 +28,6 @@ function NavBar() {
           <Route path="/alerts" element={<Alerts />} />
         </Routes>
       </BrowserRouter>
-      ;
     </>
   );
 }
